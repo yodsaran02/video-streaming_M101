24d44bdcc684a3b15613aa7b3145ca0451272620
Yodsaran Panomratanarak <70568348+yodsaran02@users.noreply.github.com>
2022-11-12 12:19:21 +0000

    add Web path

--- app.py
+++ app.py
@@ -2,8 +2,12 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-  return render_template("index.html")
+def index():
+    return render_template("index.html")
+
+@app.route("/Web/<subject>")
+def Web(subject):
+    return render_template("Web/"+subject+".html")
 
 if __name__ == "__main__":
   app.run()
