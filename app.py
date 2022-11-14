cc745697d43e8995b3b6c85e3089ca04978d17d5
Yodsaran Panomratanarak <70568348+yodsaran02@users.noreply.github.com>
2022-11-14 06:06:10 +0000

    change some

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
 from flask import Flask, render_template
+import sqlite3 as sql
 app = Flask(__name__)
 
 
 app.config["TEMPLATES_AUTO_RELOAD"] = True
+db = sql.connect("Video.db")
+cursor = db.execute("SELECT * FROM video")
+for row in cursor:
+    print(row)
 
 
 @app.route("/")
@@ -13,6 +18,13 @@ def index():
 @app.route("/Web/<subject>")
 def Web(subject):
     return render_template("Web/"+subject+".html")
+
+@app.route("/search",methods=["POST"])
+def search():
+    print("Helloword")
+
+
+
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0')
