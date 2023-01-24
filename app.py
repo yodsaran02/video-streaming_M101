7f23de1aa3ba68e504701b2e12cb3a38227b2185
Yodsaran Panomratanarak <70568348+yodsaran02@users.noreply.github.com>
2023-01-24 07:29:25 +0000

    login test1

--- app.py
+++ app.py
@@ -2,10 +2,15 @@ from flask import Flask, render_template, request, session, redirect
 import sqlite3 as sql
 import subprocess as process
 import os
+from tempfile import mkdtemp
+from werkzeug.security import check_password_hash, generate_password_hash
 app = Flask(__name__)
 
 
 app.config["TEMPLATES_AUTO_RELOAD"] = True
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
 con = sql.connect("video.db", check_same_thread=False)
 db = con.cursor()
 def execute(dbs,command):
@@ -86,6 +91,11 @@ def convert():
                     if converting[i] == tempfile[j]:
                         tempfile.remove(tempfile[j])
         return render_template("convert.html",version=version,tempfile=tempfile)
+
+
+@app.route('/login')
+def login():
+    return render_template("login.html",version=version)
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0')
