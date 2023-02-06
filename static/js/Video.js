4576d655ae28f949d2b113bf076961a06876c085
Yodsaran Panomratanarak <70568348+yodsaran02@users.noreply.github.com>
2023-02-06 14:28:58 +0000

    fix can't play video in quick access

--- static/js/Video.js
+++ static/js/Video.js
@@ -5,7 +5,7 @@ if (screen.width < 420)
 function playvideo(subject){
   var video = document.getElementById('video');
   var video_name = document.getElementById("date").value;
-  var str = "http://"+ "170.187.225.114" + ":3001/Video/"+subject+"/" + video_name;
+  var str = "http://"+ "jwind.tv" + ":3001/Video/"+subject+"/" + video_name;
   console.log(str);
   video.setAttribute("src", str);
   video.load();
